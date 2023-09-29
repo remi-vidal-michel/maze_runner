a31628563f43529f8a105e2155b33f50a13ed120
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2023-09-29 10:59:16 +0200

    [fix] errors and validation entries handling

--- src/main/java/MazeRunner.java
+++ src/main/java/MazeRunner.java
@@ -2,13 +2,48 @@ import maze.Maze;
 import maze.exceptions.MazeGenerationException;
 
 public class MazeRunner {
-    public static void main(String[] args) throws MazeGenerationException {
+    public static void main(String[] args) {
+        try {
+            if (args.length != 4) {
+                printUsage();
+                return;
+            }
 
-        int width = Integer.parseInt(args[0]);
-        int height = Integer.parseInt(args[1]);
-        String generationType = args[2];
-        String generatorMethod = args[3];
+            int width = Integer.parseInt(args[0]);
+            int height = Integer.parseInt(args[1]);
+            String generationType = args[2];
+            String generatorMethod = args[3];
 
-        new Maze(width, height, generationType, generatorMethod);
+            if (width < 5 || height < 5) {
+                System.err.println("Erreur : Veuillez fournir une largeur et une hauteur valides supérieurs à 5");
+                printUsage();
+                return;
+            }
+
+            if (!isValidGenerationType(generationType) || !isValidGeneratorMethod(generatorMethod)) {
+                System.err.println("Erreur : Veuillez fournir un type de labyrinthe et une méthode de génération valides.");
+                printUsage();
+                return;
+            }
+
+            new Maze(width, height, generationType, generatorMethod);
+        } catch (NumberFormatException e) {
+            System.err.println("Erreur : Veuillez fournir des nombres valides pour la largeur et la hauteur.");
+            printUsage();
+        } catch (MazeGenerationException e) {
+            System.err.println("Erreur inattendue lors de la génération du labyrinthe. Veuillez réessayer.");
+        }
+    }
+
+    private static void printUsage() {
+        System.err.println("Utilisation : java -jar MazeRunner.jar [largeur] [hauteur] [perfect/imperfect] [simple/graph/optimized]");
+    }
+
+    private static boolean isValidGenerationType(String type) {
+        return type.equals("perfect") || type.equals("imperfect");
+    }
+
+    private static boolean isValidGeneratorMethod(String method) {
+        return method.equals("simple") || method.equals("graph") || method.equals("optimized");
     }
 }
--- src/main/java/maze/exceptions/MazeGenerationException.java
+++ src/main/java/maze/exceptions/MazeGenerationException.java
@@ -1,7 +1,7 @@
 package maze.exceptions;
 
 public class MazeGenerationException extends Exception {
-    public MazeGenerationException(String s) {
-        super(s);
+    public MazeGenerationException(String message) {
+        super(message);
     }
 }
